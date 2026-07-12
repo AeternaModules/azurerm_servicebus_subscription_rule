1b4fd9c883b3942631dab0dee47787e4e8280e87
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:22 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "servicebus_subscription_rules" {
-  description = "All servicebus_subscription_rule resources"
-  value       = azurerm_servicebus_subscription_rule.servicebus_subscription_rules
-}
 output "servicebus_subscription_rules_action" {
-  description = "List of action values across all servicebus_subscription_rules"
-  value       = [for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : v.action]
+  description = "Map of action values across all servicebus_subscription_rules, keyed the same as var.servicebus_subscription_rules"
+  value       = { for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : k => v.action }
 }
 output "servicebus_subscription_rules_correlation_filter" {
-  description = "List of correlation_filter values across all servicebus_subscription_rules"
-  value       = [for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : v.correlation_filter]
+  description = "Map of correlation_filter values across all servicebus_subscription_rules, keyed the same as var.servicebus_subscription_rules"
+  value       = { for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : k => v.correlation_filter }
 }
 output "servicebus_subscription_rules_filter_type" {
-  description = "List of filter_type values across all servicebus_subscription_rules"
-  value       = [for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : v.filter_type]
+  description = "Map of filter_type values across all servicebus_subscription_rules, keyed the same as var.servicebus_subscription_rules"
+  value       = { for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : k => v.filter_type }
 }
 output "servicebus_subscription_rules_name" {
-  description = "List of name values across all servicebus_subscription_rules"
-  value       = [for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : v.name]
+  description = "Map of name values across all servicebus_subscription_rules, keyed the same as var.servicebus_subscription_rules"
+  value       = { for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : k => v.name }
 }
 output "servicebus_subscription_rules_sql_filter" {
-  description = "List of sql_filter values across all servicebus_subscription_rules"
-  value       = [for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : v.sql_filter]
+  description = "Map of sql_filter values across all servicebus_subscription_rules, keyed the same as var.servicebus_subscription_rules"
+  value       = { for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : k => v.sql_filter }
 }
 output "servicebus_subscription_rules_sql_filter_compatibility_level" {
-  description = "List of sql_filter_compatibility_level values across all servicebus_subscription_rules"
-  value       = [for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : v.sql_filter_compatibility_level]
+  description = "Map of sql_filter_compatibility_level values across all servicebus_subscription_rules, keyed the same as var.servicebus_subscription_rules"
+  value       = { for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : k => v.sql_filter_compatibility_level }
 }
 output "servicebus_subscription_rules_subscription_id" {
-  description = "List of subscription_id values across all servicebus_subscription_rules"
-  value       = [for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : v.subscription_id]
+  description = "Map of subscription_id values across all servicebus_subscription_rules, keyed the same as var.servicebus_subscription_rules"
+  value       = { for k, v in azurerm_servicebus_subscription_rule.servicebus_subscription_rules : k => v.subscription_id }
 }
 
